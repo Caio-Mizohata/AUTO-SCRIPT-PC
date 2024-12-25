21ead184d0e84995161b83f860c8ce2d5159315d
caioeduardo4100 <Caiomizohata@gmail.com>
2024-12-25 14:49:19 -0300

    Updated Script

--- Reparar Windows/Reparar Sistema.bat
+++ Reparar Windows/Reparar Sistema.bat
@@ -1,49 +1,30 @@
 @echo off
-:: Configura a cor do fundo e do texto para uma interface mais agradável
-color 0A
 
-:: Título da janela do terminal
-title Verificação e Restauração do Sistema
+title Verificacao e Restauracao do Sistema
 
-:: Exibe uma mensagem inicial
-echo ===============================
-echo  Verificando a Saúde do Windows
-echo ===============================
+echo Verificando a Saude do Windows
 echo.
 
-:: Inicia a verificação de saúde da imagem do Windows
-echo [1/4] Iniciando a verificação de saúde da imagem do Windows...
-echo ==============================================================
-echo.
+echo [1/4] Iniciando a verificacao de saude da imagem do Windows...
+
 DISM /Online /Cleanup-Image /ScanHealth
 echo.
 
-:: Restaura a saúde da imagem do Windows
-echo [2/4] Restaurando a saúde da imagem do Windows...
-echo =================================================
-echo.
+echo [2/4] Restaurando a saude da imagem do Windows...
+
 DISM /Online /Cleanup-Image /RestoreHealth
 echo.
 
-:: Verifica os arquivos de sistema
 echo [3/4] Verificando arquivos de sistema...
-echo ========================================
-echo.
+
 sfc /scannow
 echo.
 
-:: Verifica o disco rígido
-echo [4/4] Iniciando a verificação do disco...
-echo =========================================
-echo.
+echo [4/4] Iniciando a verificacao do disco...
+
 chkdsk /scan
+
+echo  Verificação Concluida com Sucesso!
 echo.
 
-:: Mensagem final
-echo ====================================
-echo  Verificação Concluída com Sucesso!
-echo ====================================
-echo.
-
-:: Pausa para o usuário ver os resultados
 pause
